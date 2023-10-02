9cc04e9359fbc2f91759431d037a610836a9d0c4
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2023-10-03 00:39:30 +0700

    update
    Tạo method check file input

--- DoAnLTDT/DoAnLTDT/Xuly.cs
+++ DoAnLTDT/DoAnLTDT/Xuly.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,18 @@ namespace DoAnLTDT
 {
     public class Xuly
     {
+        public int n;
+        public int[,] data;
+        
+        //Kiem tra file co ton tai hay khong
+        public bool KiemTraFile(string filename)
+        {
+            if(!File.Exists(filename))
+            {
+                Console.WriteLine("File khong ton tai!!!");
+                return false;
+            }
+            return true;
+        }
     }
 }
