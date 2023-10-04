6155f74e13b874eaf51b1010a99af46a38b96e36
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2023-10-05 01:50:25 +0700

    update
    Chuyen danh sach ke sang ma tran ke

--- DoAnLTDT/DoAnLTDT/Xuly.cs
+++ DoAnLTDT/DoAnLTDT/Xuly.cs
@@ -25,27 +25,85 @@ namespace DoAnLTDT
             return true;
         }
         //Tao va ghi file moi
-        public static void LuuTruMaTranKe(int[] MaTranKe)
+        public static void LuuTruMaTranKe(int[,] MaTranKe)
         {
+            data = new int[n +1 , n + 1];
             var filename = "App Data/data.txt";
             var filePatch = Path.GetFullPath(filename);
             using (StreamWriter fileData = new StreamWriter(filePatch))
             {
-                fileData.WriteLine(n);
+                fileData.WriteLine(n + 1);
+                for(int i = 0; i < n + 1; i++)
+                {
+                    for (int j = 0; j < n + 1; j++)
+                    {
+                        data[i, j] = MaTranKe[i, j];
+                        fileData.Write(data[i, j]);
+                        
+                    }
+                    fileData.WriteLine();
+                }
                 
             }
        }
+
+        //code loi
+        //public static void ChuyenDoiMTK(string filename)
+        //{
+
+        //    int[,] newDuLieu = new int[n + 1, n + 1];
+        //    string[] file = File.ReadAllLines(filename);
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        string[] tokens = file[i + 1].Split(' ');
+                
+                    
+                
+
+        //        for (int tokensCount = 1; tokensCount < tokens.Length; tokensCount = tokensCount + 2)
+        //        {
+        //            for (int j = 0; j < n; j++)
+        //            {
+        //                newDuLieu[i, j] = int.Parse(tokens[tokensCount]);
+
+        //            }
+        //        }
+        //    }
+        //    LuuTruMaTranKe(newDuLieu);
+
+        //}
         //Chuyen doi danh sach ke sang ma tran ke
         public static void ChuyenDoiMTK(string filename)
         {
+        
+        int[,] newDuLieu = new int[n + 1, n + 1];
+                string[] file = File.ReadAllLines(filename);
+            for (int i = 0; i<n; i++)
+            {
+                string[] tokens = file[i + 1].Split(' ');
 
+                for (int tokensCount = 1; tokensCount<tokens.Length; tokensCount = tokensCount + 2)
+                {
+                    int vertex = int.Parse(tokens[tokensCount]);
+                    //Console.WriteLine(vertex);
+                    //Console.WriteLine("---");
+                    // Lưu đỉnh vào ma trận kề
+                    newDuLieu[i, vertex] = 1; // Giả sử đỉnh được đánh số từ 1, nếu đánh số từ 0 thì chỉ cần newDuLieu[i, vertex] = 1;
+                   
+
+                }
+            }
+                LuuTruMaTranKe(newDuLieu);
             
 
         }
         //check output array
-        public static void OutputArray(int[] arr)
-        {
-            
-        }
+        //public static void OutputArray(int[,] arr)
+        //{
+        //    foreach(int i in arr)
+        //    {
+        //        Console.Write(i);
+        //    }
+        //}
     }
 }
